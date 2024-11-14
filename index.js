d4295e311867780e8bebd0020d85b5af9d942070
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-11-14 17:03:44 +0630

    object destructuring

---
+++ index.js
@@ -0,0 +1,26 @@
+const dreamHoliday = {
+  destination: "Austin, Texas",
+  activity: "visit the Tesla HQ",
+  accommodation: "luxury ranch",
+  companion: "Elon Musk",
+};
+
+const { destination, activity, accommodation, companion } = dreamHoliday;
+
+console.log(
+  `I would love to go to ${destination} to ${activity}. I'd sleep in a ${accommodation} and hang out with ${companion} all day.`
+);
+
+const favoriteFilm = {
+  title: "Top Gun",
+  year: "1986",
+  genre: "action",
+  star: "Tom Cruise",
+  director: "Tony Scott",
+};
+
+const { title, year, genre, star, director } = favoriteFilm;
+
+console.log(
+  `My favorite film is ${title} starring ${star}. It's an ${genre} film that was directed by ${director} and released in ${year}.`
+);
